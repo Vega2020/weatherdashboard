ca99483d34611b9ae14976bbc2ca824a76478af2
Jeremy Flowers <flowersje08@gmail.com>
2020-05-17 14:16:24 -0700

    jumbotron and ajax call working

--- script.js
+++ script.js
@@ -13,6 +13,43 @@
 // THEN I am presented with the last searched city forecast
 
 
+//code from class bujumbura example
+    // This is my API key for the weather info service.
+    var APIKey = "84733b4b40205836872f7d15403843d8";
+
+    //create a variable for the city input by the user and add it to the queryURL
+
+    // Here we are building the URL we need to query the database. we'll change this (or maybe make additional ones) for current project. We will have to create functions that turn user search parameters into variables and then append those variables to the url. There's code for this in the news article group project. Change it to backticks so you can use jquery to build the url and delete the stuff about burundi.
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=Bujumbura,Burundi&appid=" + APIKey;
+
+    // We then create an AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      // this logs the query url and response object for test purposes
+      console.log(queryURL);
+      console.log(response);// "response" could as easily be "burrito?" test this out when code works more
+
+      // Create CODE HERE to transfer content to HTML
+      // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
+      var city = response.name;
+      var windSpeed = response.wind.speed;
+      var humidity = response.main.humidity;
+      var temp = response.main.temp;
+
+      // Then we have our divs display that as its text.
+      $(".city").text(city);//make this div display the text bigger, add today's date (get code from calendar assignment) and the weather icon
+      $(".wind").text("Wind speed: " + windSpeed);
+      $(".humidity").text("Humidity: " + humidity + "%");//look up the syntax to do this with backticks and curly brackets instead of pluses
+      $(".temp").text(temp);
+
+      // this function puts out an ajax call to openweather and then returns the data and assigns variables to certain pieces of it. the weather dashboard will use this procedure to get variables. we need to create forms by which the user can tell the page what cities to get information about, and then create functions that will load the information onto the browser window.
+
+
+    });//this is the closing bracket for the ajax call.
+
 
 //code from class demo
 var APIKey = "your api key here";
