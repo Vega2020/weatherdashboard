d4ec23245776e612560f574e3bd4443974b900ce
Jeremy Flowers <flowersje08@gmail.com>
2020-05-17 18:18:30 -0700

    main search bar and jumbotron are working, next step is saving searches to local memory

--- script.js
+++ script.js
@@ -21,45 +21,54 @@
 //create a variable for the city input by the user and add it to the queryURL:
     var query = $("#search-input").val();
 
-    // Here we are building the URL we need to query the database. we'll change this (or maybe make additional ones) for current project. We will have to create functions that turn user search parameters into variables and then append those variables to the url. There's code for this in the news article group project. Change it to backticks so you can use jquery to build the url and delete the stuff about burundi.
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${query}&appid=${APIKey}`;
-
-    // We then create an AJAX call
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-
-      // this logs the query url and response object for test purposes
-      console.log(queryURL);
-      console.log(response);// "response" could as easily be "burrito?" test this out when code works more
-
-      // Create CODE HERE to transfer content to HTML
-      // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
-      var city = response.name;
-      var windSpeed = response.wind.speed;
-      var humidity = response.main.humidity;
-      var temp = response.main.temp;
-
-      // Then we have our divs display that as its text.
-      $(".city").text(city);//make this div display the text bigger, add today's date (get code from calendar assignment) and the weather icon
-      $(".wind").text("Wind speed: " + windSpeed);
-      $(".humidity").text("Humidity: " + humidity + "%");//look up the syntax to do this with backticks and curly brackets instead of pluses
-      $(".temp").text(temp);
-
-      // this function puts out an ajax call to openweather and then returns the data and assigns variables to certain pieces of it. the weather dashboard will use this procedure to get variables. we need to create forms by which the user can tell the page what cities to get information about, and then create functions that will load the information onto the browser window.
-
-
+    
+    
+    //this is the function for the search button
+    $("#searchButton").on("click", function(e) {
+        
+        // this variable represents the input from the search bar. This line has to be above the variable below, otherwise the queryURL comes back as undefined!
+        var userInput = $("#search-input").val().trim();
+        
+        // Here we are building the URL we need to query the database. we'll change this (or maybe make additional ones) for current project. We will have to create functions that turn user search parameters into variables and then append those variables to the url. There's code for this in the news article group project. use bacticks and curly brackets to build the url in jquery. this variable needs to exist inside the on click function because the jquery in the url itself needs to be able to see the variables it's referring to?
+        var queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userInput}&appid=${APIKey}`;
+        
+        // We create an AJAX call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
+            
+        // this logs the query url and response object for test purposes
+        console.log(queryURL);
+        console.log(response);// "response" could as easily be "burrito?" test this out when code works more
+        
+        // Create CODE HERE to transfer content to HTML
+        // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
+        var city = response.name;
+        var windSpeed = response.wind.speed;
+        var humidity = response.main.humidity;
+        var temp = response.main.temp;
+        
+        // Then we have our divs display that as its text.
+        $(".city").text(city);//make this div display the text bigger, add today's date (get code from calendar assignment) and the weather icon
+        $(".wind").text("Wind speed: " + windSpeed);
+        $(".humidity").text("Humidity: " + humidity + "%");//look up the syntax to do this with backticks and curly brackets instead of pluses
+        $(".temp").text(temp);
+        
+        // this function puts out an ajax call to openweather and then returns the data and assigns variables to certain pieces of it. the weather dashboard will use this procedure to get variables. we need to create forms by which the user can tell the page what cities to get information about, and then create functions that will load the information onto the browser window.
+        
+        
     });//this is the closing bracket for the ajax call.
-
-
-//code from class demo
-// var APIKey = "your api key here";
-// function getWeather(city) {
-//     var queryURL= `https://api.openweathermap.org/data/2.5/weather?q=$`
-// };
-
-// function getUv(lat, lon) {
+    
+});//this is the closing bracket for the search button function
+    
+    //code from class demo
+    // var APIKey = "your api key here";
+    // function getWeather(city) {
+        //     var queryURL= `https://api.openweathermap.org/data/2.5/weather?q=$`
+        // };
+        
+        // function getUv(lat, lon) {
 //     const queryUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&long={lon}`;
 //     $.ajax({
 //         url: queryURL,
