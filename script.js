193022e3a967057c4de707ca2ec9f5c51065f787
Jeremy Flowers <flowersje08@gmail.com>
2020-05-18 16:12:10 -0700

    icon working, trying to get history to work

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
@@ -13,74 +12,77 @@
 // THEN I am presented with the last searched city forecast
 
 //this gets today's date from moment.js and stores it as a variable
-    const todaysDate = moment().format("MMM DD YYYY");
+const todaysDate = moment().format("MM/DD/YYYY");
 
 // This is my API key for the weather info service.
-    var APIKey = "84733b4b40205836872f7d15403843d8";
+var APIKey = "84733b4b40205836872f7d15403843d8";
 
 //create a variable for the city input by the user and add it to the queryURL:
-    var query = $("#search-input").val();
+var query = $("#search-input").val();
 
+var historyList = ["boston", ];
+
+var history = localStorage.getItem("history");
+
+// this line is supposed to push everything in the history into the historyList array, but it throws an error saying push isn't a function
+//var newHistory = history.push(historyList);
+
+//this is the beginning of the main call weather function
+function callWeather(city) {
     
+    // Here we are building the URL we need to query the database. We will have to create functions that turn user search parameters into variables and then append those variables to the url. this variable needs to exist inside the on click function because the jquery in the url itself needs to be able to see the variables it's referring to?
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIKey}`;
     
-    //this is the function for the search button
-    $("#searchButton").on("click", function(e) {
+    // We create an AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
         
-        // this variable represents the input from the search bar. This line has to be above the variable below, otherwise the queryURL comes back as undefined!
-        var userInput = $("#search-input").val().trim();
-        
-        // Here we are building the URL we need to query the database. we'll change this (or maybe make additional ones) for current project. We will have to create functions that turn user search parameters into variables and then append those variables to the url. There's code for this in the news article group project. use bacticks and curly brackets to build the url in jquery. this variable needs to exist inside the on click function because the jquery in the url itself needs to be able to see the variables it's referring to?
-        var queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${userInput}&appid=${APIKey}`;
-        
-        // We create an AJAX call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            
-        // this logs the query url and response object for test purposes
-        console.log(queryURL);
-        console.log(response);// "response" could as easily be "burrito?" test this out when code works more
-        
-        // Create CODE HERE to transfer content to HTML
+    // this logs the response object for test purposes
+    console.log(response);// "response" could as easily be "burrito?" test this out when code works more
+
         // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
-        var city = response.name;
-        var windSpeed = response.wind.speed;
-        var humidity = response.main.humidity;
-        var temp = response.main.temp;
-        
-        // Then we have our divs display that as its text.
-        $(".city").text(city);//make this div display the text bigger, add today's date (get code from calendar assignment) and the weather icon
-        $(".wind").text("Wind speed: " + windSpeed);
-        $(".humidity").text("Humidity: " + humidity + "%");//look up the syntax to do this with backticks and curly brackets instead of pluses
-        $(".temp").text(temp);
-        
-        // this function puts out an ajax call to openweather and then returns the data and assigns variables to certain pieces of it. the weather dashboard will use this procedure to get variables. we need to create forms by which the user can tell the page what cities to get information about, and then create functions that will load the information onto the browser window.
-        
-        
-    });//this is the closing bracket for the ajax call.
+    var city = response.name;
+    var windSpeed = response.wind.speed;
+    var humidity = response.main.humidity;
+    var temp = response.main.temp;
+
+    historyList.push(city);//will have to add if-else function to prevent cities from duplicating on list
+    localStorage.setItem("history", historyList);
+    console.log(historyList);
+
+    // this saves the searched city to local storage to be accessed when the page loads
+    localStorage.setItem("lastSearched", city);
     
-});//this is the closing bracket for the search button function
+    // Then we have our divs display that as its text.
+    $(".city").text(city + " " + todaysDate);
+    $("#weatherIconDisplay").html($("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"));
+    $(".wind").text("Wind speed: " + windSpeed);
+    $(".humidity").text("Humidity: " + humidity + "%");
+    $(".temp").text(temp);
+
     
-    //code from class demo
-    // var APIKey = "your api key here";
-    // function getWeather(city) {
-        //     var queryURL= `https://api.openweathermap.org/data/2.5/weather?q=$`
-        // };
-        
-        // function getUv(lat, lon) {
-//     const queryUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&long={lon}`;
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET",
-//     })
-//     .then(function (response) {
-//         console.log(response);
-//     });
-// }
+});//this is the closing bracket for the ajax call.
 
-// function fiveDayForecast(lat, lon){
+}//this is the closing bracket for the callWeather function.
 
-// }
+//this is the function for the search button
+$("#searchButton").on("click", function() {
+    var city = $("#search-input").val().trim();
+    callWeather(city);
+});
 
-// getWeather("Gilbert");
+
+
+
+//this gets the most recent city from local storage and displays it when the page loads
+var mostRecent = localStorage.getItem("lastSearched")
+callWeather (mostRecent);
+
+//code from class for building the history
+previousCities = []
+localStorage.setItem("history", JSON.stringify(previousCities));
+
+
+
