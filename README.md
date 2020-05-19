abad7b5b2b18302b003996c91261ca29e6c6cd8e
Jeremy Flowers <flowersje08@gmail.com>
2020-05-18 21:46:23 -0700

    history column functional, have to get buttons to work

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ back end:
 1 - commit 3 or so. Cannot get bootstrap navbar to center. Successfully have main row and columns working, now need to add bootstrap component classes to them. I have found that inspecting the code of tutorial pages that use bootstrap is as helpful as actually reading the tutorials. my next step is to work on adding bootstrap components to the columns I just created.
 
 2 - commit 6 or so. have improved jumbotron. working on building the query for the api; need to create a button to go with the search bar, then set a variable in js file for the user input, and then build it into the api url with jquery. this is why the jumbotron is currently broken, it will work again when the code to build the query url has been fixed.
+
+3 - commit ~12. got help in class with some syntax. Small details make a difference. Through using google, annotation to make sure I understand each piece of my code, and lots of testing I have gotten most of the funtionality to work and have learned a lot about integrating ajax calls and functions in general into javascript and outputting the results to html, which are the key skills for integrating the front and back end.
--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name (check), the date(check), an icon representation of weather conditions(check), the temperature(check), the humidity(check), the wind speed(Check), and the UV index
+// WHEN I search for a city THEN I am presented with current and future conditions for that city and that city is added to the search history (check)
+// WHEN I view current weather conditions for that city THEN I am presented with the city name (check), the date(check), an icon representation of weather conditions(check), the temperature(check), the humidity(check), the wind speed(Check), and the UV index
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city forecast
+// WHEN I open the weather dashboard THEN I am presented with the last searched city forecast (check)
 
 //this gets today's date from moment.js and stores it as a variable
 const todaysDate = moment().format("MM/DD/YYYY");
@@ -20,8 +17,6 @@ var APIKey = "84733b4b40205836872f7d15403843d8";
 //create a variable for the city input by the user and add it to the queryURL:
 var query = $("#search-input").val();
 
-//there was an empty array declared historylist here, making the functions below work. deleted it to avoid declaring the historylist as an empty function. must save and load it only out of local memory to avoid it resetting itself every time.
-
 var history = localStorage.getItem("history");
 
 //this is the beginning of the main call weather function
@@ -29,6 +24,7 @@ function callWeather(city) {
     
     // Here we are building the URL we need to query the database. We will have to create functions that turn user search parameters into variables and then append those variables to the url. this variable needs to exist inside the on click function because the jquery in the url itself needs to be able to see the variables it's referring to?
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q=${city}&appid=${APIKey}`;
+    var FivedayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`;
     
     // We create an AJAX call
     $.ajax({
@@ -39,27 +35,24 @@ function callWeather(city) {
     // this logs the response object for test purposes
     console.log(response);// "response" could as easily be "burrito?" test this out when code works more
 
-        // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
+    // What's going on here is the response is an object, and we're declaring variables that are pulled from keys of that object.
     var city = response.name;
     var windSpeed = response.wind.speed;
     var humidity = response.main.humidity;
     var temp = response.main.temp;
     
-    //LOCAL STORAGE CODE FROM INTERNET
-    // Get the existing data
+    // This retrieves our search history and sets it to a variable. The history data's name in local storage is set by the setItem method below.
     var existing = localStorage.getItem("history");
-
-    // If no existing data, create an array
-    // Otherwise, convert the localStorage string to an array
+    // If no existing data, create an array. Otherwise, convert the localStorage string to an array.
     existing = existing ? existing.split(',') : [];
-
     // Add new data to localStorage Array. The if statement prevents cities from being duplicated!
     if (!existing.includes(city)) existing.push(city);
 
+    // logs our list of cities for testing
     console.log(existing);
-    // Save back to localStorage
+
+    // Save the new history data back to localStorage to use later.
     localStorage.setItem("history", existing.toString());
-    //END LOCAL STORAGE CODE FROM INTERNET
 
     // this saves the searched city to local storage to be accessed when the page loads
     localStorage.setItem("lastSearched", city);
@@ -71,17 +64,36 @@ function callWeather(city) {
     $(".humidity").text("Humidity: " + humidity + "%");
     $(".temp").text("Temperature: " + temp);
 
+        historyColumn();
     
-});//this is the closing bracket for the ajax call.
+    });//this is the closing bracket for the main ajax call.
+
+    //NEED: add an ajax call for the 5 day forecast data and fill the cards with it
 
 }//this is the closing bracket for the callWeather function.
 
-//this is the function for the search button
+//NEED: write code to generate html elements for each city in our local history and display them under the search bar
+//first write a for each loop for the local history variable that makes each city an <li> and appends it to the #recentlySearchedList div.
+
+// this function should add each item in the searched city list to be appended to the history column in li form. It works!
+function historyColumn() {
+    var history = localStorage.getItem("history");
+    console.log(history);
+    var historyList = history.split(",");
+    console.log(historyList);
+    historyList.forEach(putInHistory);
+
+    function putInHistory(item) {
+       $("#recentlySearchedList").append("<li>" + item + "</li>")//NEXT: fix this .append function to make the li into a box that activates when clicked
+    };//closing bracket for putInHistory function
+
+};//closing bracket for historycolumn function
+
+//this is the function for the search button. It would be good to make it operate on form submission instead.
 $("#searchButton").on("click", function() {
     var city = $("#search-input").val().trim();
     callWeather(city);
 });
-
 
 //this gets the most recent city from local storage and displays it when the page loads
 var mostRecent = localStorage.getItem("lastSearched")
